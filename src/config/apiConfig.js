c562d2784ea12a1ba724b783c9334b5654932779
Cristian Aldair Alcantara Anton <cris.alcantara.anton@gmail.com>
2025-10-03 05:55:30 -0500

    despliegue v2 api azure

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = 'https://localhost:7227/api'
+//export const API_BASE_URL = 'https://localhost:7227/api'
+export const API_BASE_URL =
+  "https://postulate-api-engufne4e7c9cmdy.brazilsouth-01.azurewebsites.net";
